af728bdbd81bace24d2d69279a46bd55aadcbdb5
ssaeidd <saeid.basirnia@gmail.com>
2018-01-16 19:46:15 +0330

    Initial Commit

---
+++ SwiftyGuideOverlay/SwiftyGuideOverlay.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyGuideOverlay.h
+//  SwiftyGuideOverlay
+//
+//  Created by Saeid Basirnia on 1/16/18.
+//  Copyright © 2018 Saeid Basirnia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyGuideOverlay.
+FOUNDATION_EXPORT double SwiftyGuideOverlayVersionNumber;
+
+//! Project version string for SwiftyGuideOverlay.
+FOUNDATION_EXPORT const unsigned char SwiftyGuideOverlayVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyGuideOverlay/PublicHeader.h>
+
+
